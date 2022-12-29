122743ce701d7df1336e001b2cc4d0a43666fdb4
lee Jeong Eun <wjd7dms7@naver.com>
2022-12-29 16:43:16 +0900

    서브라우터 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 const App = () => {
   return (
@@ -14,13 +15,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
       </ul>
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />  
+        {/* <Route path="/profiles/:username" element={<Profile />} />   */}
         {/* // username 에 해당하는 값을 파라미터로 넣어주어서 Profile 컴포넌트에서 match props 를 통하여 전달받을 수 있게 됨. */}
+        <Route path="/profiles/*" element={<Profiles />} />  
       </Routes>
     </>
   );
